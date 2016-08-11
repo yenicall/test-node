b0059ba6d138c6d0dff83c2ea94c0385275be29d
Pedro Mangabeira <pedro.mangabeira@teacherfits.me>
2016-08-11 21:50:13 +0200

    rs

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const mongoose = require('mongoose')
 const uuid = require('uuid')
 throng(function (id) {
   console.log(`Started worker ${id}`)
-  mongoose.connect('mongodb://mongo:27017/test')
+  mongoose.connect('mongodb://mongo:27017/test', {replset: {rs_name: 'rs0'}})
   let Cats = mongoose.model('Cats', mongoose.Schema({ name: String }))
   let app = express()
   app.get('/', (req, res) => {
     res.send('hello world')
   })
   app.get('/cats', (req, res) => {
-    Cats.find({}, (e, r) =>  res.send(JSON.stringify({e,r})))
+    Cats.find({}, (e, r) => res.send(JSON.stringify({e,r})))
   })
   app.get('/cats/p', (req, res) => {
     let cat = new Cats({name: uuid.v4()})
     cat.save((e, r) => res.send(JSON.stringify({e,r})))
   })
-  app.listen(3000,()=>console.log('listen'))
+  app.listen(3000, () => console.log('listen'))
 })
